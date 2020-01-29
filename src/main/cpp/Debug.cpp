e9f5743e3e83b76fef30dcc6eabb1ca417c87368
Matthew VandenBoom <Bookster99@outlook.com>
2020-01-29 14:46:03 -0600

    Added Debug.h and .cpp to github

---
+++ src/main/cpp/Debug.cpp
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Debug.h"
+
+Debug::Debug(std::string name) 
+{
+    table = nt::NetworkTableInstance::GetDefault().GetTable(name);
+}
+
+void Debug::PutString(std::string key, std::string value)
+{
+    table->PutString(key, value);
+}
+
+void Debug::PutNumber(std::string key, float value)
+{
+    table->PutNumber(key, value);
+}
---
+++ src/main/include/Debug.h
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
+class Debug {
+ public:
+  Debug(std::string name);
+
+  void PutString(std::string key, std::string value);
+  void PutNumber(std::string key, float value);
+
+
+  std::shared_ptr<NetworkTable> table;
+};
